85d3cf649a5932d317a23c2d58155ec29d243994
Oye Oloyede <oyekunlemac@gmail.com>
2024-05-16 22:07:31 +0100

    revamped setup

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,10 +1,18 @@
 #!/bin/zsh
 
-# create symbolic links to zsh, vim and tmux conf files
-ln -nfs $HOME/my_works/dotfiles/.zshrc $HOME/.zshrc
-ln -nfs $HOME/my_works/dotfiles/.vimrc $HOME/.vimrc
+#  zsh
+cat ./zsh/common > ~/.zshrc
+cat ./zsh/`$1` >> ~/.zshrc
+cat ./zsh/.private >> ~/.zshrc
+echo "alias dot_g='/usr/bin/git --git-dir=`realpath ./git` --work-tree=`realpath .`'" >> ~/.zshrc
+
+# tmux
+# tmux sym link
 ln -nfs $HOME/my_works/dotfiles/.tmux.conf $HOME/.tmux.conf
 
+# vim
+# vim symbolic link
+ln -nfs $HOME/my_works/dotfiles/.vimrc $HOME/.vimrc
 # vim expects some folders to exist in the home directory, create those if they don't
 mkdir -p $HOME/.vim
 mkdir -p $HOME/.vim/.undo
