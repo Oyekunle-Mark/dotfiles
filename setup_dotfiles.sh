8c88e7c0c0302caaae8d5730fe86bb9aae7462bd
Oye Oloyede <oyekunlemac@gmail.com>
2024-05-16 22:53:20 +0100

    Added check for parameter and optional checks for .private

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,9 +1,20 @@
 #!/bin/zsh
 
+
+if [[ -z $1 ]]; then
+	echo  "Usage ./setup_dotfiles.sh <environment>"
+	echo "<environment> must match the file name with environment specific configs"
+	exit 1
+fi
+
+
 #  zsh
 cat ./zsh/common > ~/.zshrc
 cat ./zsh/$1 >> ~/.zshrc
-cat ./zsh/.private >> ~/.zshrc
+
+if [[ -e "./zsh/.private" ]]; then
+	cat ./zsh/.private >> ~/.zshrc
+fi
 
 
 # tmux
