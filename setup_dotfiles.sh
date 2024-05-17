d3b2ecbdfe8cd703c295087dc41d492c5b5c4992
Oye Oloyede <oyekunlemac@gmail.com>
2024-05-17 21:55:40 +0100

    used a function to abstract all the reusable parts

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,31 +1,63 @@
 #!/bin/zsh
 
-
 if [[ -z $1 ]]; then
-	echo  "Usage ./setup_dotfiles.sh <environment>"
-	echo "<environment> must match the file name with environment specific configs"
+	echo "Usage ./setup_dotfiles.sh <environment>"
+	echo "Where <environment> is the name of the environment specific config files for this machine.\n"
 	exit 1
 fi
 
+echo "\nBuilding config files for the $1 environment...\n"
+
+# builds a config file by merging
+# $1 => folder that contains the config files for a utility
+# $2 => environment
+# $3 => the final config file as a path to where it should be located
+#
+# 1: common (which represents the common config we want in all environments for this utilility)
+# 2: <environment> (which represents the environment specific config specific to the machine we run this script on for this utility)
+# 3: .private (sensitive information we do not check into git and share with other machines)
+build_file() {
+	echo "\nBuilding $3...\n"
+	rm -f $3
+	touch $3
+
+	if [[ -e "./$1/common" ]]; then
+		echo "Merging ./$1/common into $3..."
+		cat ./$1/common >> $3
+	else
+		echo "Could not find ./$1/common. Skipping..."
+	fi
+
+	if [[ -e "./$1/$2" ]]; then
+		echo "Merging ./$1/$2 into $3..."
+		cat ./$1/$2 >> $3
+	else
+		echo "Could not find the ./$1/$2 environment specific file. Skipping..."
+	fi
+
+	if [[ -e "./$1/.private" ]]; then
+		echo "Merging the environement specific private file ./$1/.private into $3..."
+		cat ./$1/.private >> $3
+	else
+		echo "Could not find a .private file in ./$1. Skipping..."
+	fi
+
+	echo "$3 built successfully.\n"
+}
+
 
 #  zsh
-cat ./zsh/common > ~/.zshrc
-cat ./zsh/$1 >> ~/.zshrc
-
-if [[ -e "./zsh/.private" ]]; then
-	cat ./zsh/.private >> ~/.zshrc
-fi
-
+build_file zsh $1 ~/.zshrc
 
 # tmux
-cat ./tmux/common > ~/.tmux.conf
+build_file tmux $1 ~/.tmux.conf
 
-
-# vim
-cat ./vim/common > ~/.vimrc
 
 # vim expects some folders to exist in the home directory, create those if they don't
 mkdir -p $HOME/.vim
 mkdir -p $HOME/.vim/.undo
 mkdir -p $HOME/.vim/.backup
 mkdir -p $HOME/.vim/.swp
+
+# vim
+build_file vim $1 ~/.vimrc
